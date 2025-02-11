71b28dad50e9dbf52e5ada18be999d5bcea2b17f
kasperis3 <kushaal@berkeley.edu>
2025-02-11 14:45:11 -0800

    redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import MarketingPage from "../marketing-page/MarketingPage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <MarketingPage />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
