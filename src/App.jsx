ffd113eb62c506de7aba45947bf9cfdee79c9870
kasperis3 <kushaal@berkeley.edu>
2025-02-12 00:33:55 -0800

    initial set of working changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,27 @@ function App() {
     <>
       <div>
         <a href="">
-          <img src={commloan_logo} className="react logo" alt="CommLoan logo" />
+          <img src={commloan_logo} className="logo" alt="CommLoan logo" />
         </a>
       </div>
-      <h1>Commercial Mortgage Options</h1>
+      <h1>
+        My name is Kushaal. I'm an experienced commercial mortgage broker. I
+        studied Mathematics at UC Berkeley and I personally invest in CRE.
+      </h1>
+      <p>
+        CommLoan is a debt marketplace powered by technology and built with over
+        750 lenders nationwide. We have created a real time database where our
+        lenders update what their credit committee buy box is every 10 days.
+        What makes us unique is that we are lender neutral, we create friendly
+        competition between the lenders interested in your deal to get our
+        borrowers the best rate and terms for their specific goals.
+      </p>
+
       <div>
         <FilloutPopupEmbedButton
           filloutId="js1DZCG8E4us"
           inheritParameters
-          text="Fill questionnaire"
+          text="I'm ready for loan options"
           color="green"
           size="large"
           float="Middle"
