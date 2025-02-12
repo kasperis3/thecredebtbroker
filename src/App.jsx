f3174698eef1f7873c07a535b77a0df1b2f49b1b
kasperis3 <kushaal@berkeley.edu>
2025-02-11 23:16:46 -0800

    use fillout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "@fillout/react/style.css";
+import { FilloutStandardEmbed, FilloutFullScreenEmbed } from "@fillout/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -17,19 +17,20 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div
+        style={{
+          width: 800,
+          height: 800,
+        }}
+      >
+        <FilloutFullScreenEmbed filloutId="js1DZCG8E4us" inheritParameters />
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
