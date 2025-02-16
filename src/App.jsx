27f290f729ab750f6f6b23e3d1a8176fccc2225f
kasperis3 <kushaal@berkeley.edu>
2025-02-15 23:31:28 -0800

    add logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import commloan_logo from "./assets/commloan_logo.jpeg";
+import commloan_logo_MAIN from "./assets/commloan_logo_MAIN.png";
+import WP1 from "./assets/WP1.png";
 import "@fillout/react/style.css";
 import { FilloutPopupEmbedButton } from "@fillout/react";
 
@@ -9,15 +11,15 @@ function App() {
       <header className="mb-8">
         <a href="#" className="inline-block">
           <img
-            src={commloan_logo}
-            className="w-32 md:w-32 rounded-lg shadow-xl transition-transform duration-300 hover:scale-105"
+            src={commloan_logo_MAIN}
+            className="w-136 md:w-136 rounded-lg transition-transform duration-300 hover:scale-105"
             alt="CommLoan logo"
           />
         </a>
       </header>
 
       {/* Main Content */}
-      <main className="bg-white max-w-2xl p-8 rounded-2xl shadow-xl text-center">
+      <main className="bg-white max-w-2xl p-8 rounded-2xl shadow-xl text-center transition-transform duration-300 hover:scale-105">
         <h1 className="text-4xl font-extrabold text-gray-900 mb-4">
           Get the Best Commercial Loan—Faster & Smarter.
         </h1>
@@ -77,7 +79,7 @@ function App() {
             text="Get Your Best Loan Options Now"
             color="green"
             size="large"
-            className="px-6 py-3 bg-green-600 text-white font-semibold rounded-lg shadow-lg transition-transform duration-300 hover:scale-105 hover:bg-green-700 focus:ring focus:ring-green-300"
+            className="px-6 py-3 bg-green-600 text-white font-semibold rounded-lg shadow-lg transition-all duration-300 hover:scale-105 hover:bg-green-700 focus:ring focus:ring-green-300 hover:shadow-2xl hover:shadow-green-500/60"
           />
         </div>
 
@@ -88,6 +90,10 @@ function App() {
           <p className="mt-2 text-sm text-gray-600">
             NMLS -1597276 | Licensing - Arizona: 0943012; California: 02048126
           </p>
+        </div>
+
+        <div className="mt-8">
+          <img src={WP1} alt="CommLoan Banner" className="w-full h-auto" />
         </div>
       </main>
     </div>
